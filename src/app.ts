844010343782c285c2c9d902003ea17b87d9bd0d
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-16 16:09:22 +0100

    added routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express from "express";
 import config from "config";
 import connect from "./utils/connectDB";
 import logger from "./utils/logger";
+import routes from "./routes";
 
 const app = express();
 const port = config.get<number>('port');
 
 app.listen(port, () => {
     connect();
+    routes(app);
     logger.info(":>>>>>>>>>> App is running.");
 })
