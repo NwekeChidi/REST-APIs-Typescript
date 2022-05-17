e38809b62d13d840adfbc3c10ef9c002ac37d2e6
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-05-17 03:38:15 +0100

    user model

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
     port: 3002,
-    dbUrl: "mongodb://localhost:27017/rest-api-typescript"
+    dbUrl: "mongodb://localhost:27017/rest-api-typescript",
+    salt: 10
 }
---
+++ src/models/user.model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import config from "config";
+
+
+// typescript definition for schema
+export interface UserInput {
+    email: string;
+    name: string;
+    password: string;
+}
+
+export interface UserDocument extends UserInput, mongoose.Document {
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword(pwd: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: true,
+});
+
+// Add PreSave hook
+userSchema.pre("save", async function(next){
+    let user = this as UserDocument;
+
+    if (!user.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(config.get<number>("salt"));
+    const hash = await bcrypt.hash(user.password, salt);
+
+    user.password = hash;
+    return next();
+});
+
+// Instance methods
+userSchema.methods.comparePassword = async function(pwd: string): Promise<boolean> {
+    const user = this as UserDocument;
+
+    return bcrypt.compare(pwd, user.password)
+        .catch((e) => false);
+}
+
+const User = mongoose.model("User", userSchema);
+export default User;
